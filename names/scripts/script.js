b775870904610751bbdd6e3072e77a8d9ae63519
Mendoza Diego <diegomendoza.ing@gmail.com>
2023-01-06 09:42:29 -0500

    Finalizo código de job generator

--- names/scripts/script.js
+++ names/scripts/script.js
@@ -1,7 +1,39 @@
-const containerElement = document.getElementById("title")
+const containerElement = document.getElementById("title");
 
-containerElement.innerHTML= `
+let jobs = [
+  "Project Manager",
+  "Business Analyst",
+  "Project Coordinator",
+  "Web Developer",
+  "Computer Nerd",
+];
 
-<h1>I am a Project Manager</h1>
+let jobIndex = 0;
 
-`
+let indexLetter = 0;
+
+
+function updateJob(){
+
+  indexLetter++;
+
+  containerElement.innerHTML= `
+  <h1>I am a ${jobs[jobIndex].slice(0,indexLetter)}</h1>
+  `;
+  
+  if (indexLetter === jobs[jobIndex].length){
+    setTimeout(function(){
+      jobIndex++;
+      indexLetter = 0;
+    },250)
+  }
+
+  setTimeout(updateJob,120)
+
+  if (jobIndex === jobs.length){
+    jobIndex = 0;
+  }
+
+};
+
+updateJob();
