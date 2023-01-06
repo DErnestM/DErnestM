5da318cd10eb26c4b6b1a6f2f254a20f54507f28
Mendoza Diego <diegomendoza.ing@gmail.com>
2023-01-06 10:11:43 -0500

    Corrijo bug que no permitia el bucle infinito

--- names/scripts/script.js
+++ names/scripts/script.js
@@ -1,11 +1,11 @@
 const containerElement = document.getElementById("title");
 
 let jobs = [
-  "Project Manager",
-  "Business Analyst",
-  "Project Coordinator",
-  "Web Developer",
-  "Computer Nerd",
+  " Project Manager  ",
+  " Business Analyst  ",
+  " Project Coordinator  ",
+  " Web Developer  ",
+  " Computer Nerd  ",
 ];
 
 let jobIndex = 0;
@@ -16,23 +16,22 @@ let indexLetter = 0;
 function updateJob(){
 
   indexLetter++;
-
+  
   containerElement.innerHTML= `
   <h1>I am a ${jobs[jobIndex].slice(0,indexLetter)}</h1>
   `;
   
   if (indexLetter === jobs[jobIndex].length){
-    setTimeout(function(){
-      jobIndex++;
-      indexLetter = 0;
-    },250)
+    jobIndex++;
+    indexLetter = 0;
   }
-
-  setTimeout(updateJob,120)
-
+  
+  
   if (jobIndex === jobs.length){
     jobIndex = 0;
   }
+  
+  setTimeout(updateJob,120);
 
 };
 
